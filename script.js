098f1e413c7c67642f54ac32049057d82d1c5dbd
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2024-11-21 21:50:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function getFormvalue() {
     //Write your code here
+	const first = document.getElementsByName("fname")[0];
+	const last = document.getElementsByName("lname")[0];
 
+	let firstname= first.value;
+	let lastname = last.value;
+
+	let result = (firstname+" "+lastname);
+	
+	alert(result);	
 }
+
+
+
+
+
